b0fd0558ef28cb7a2b8ae41a2aaf6c91ff8d9c94
lawass1864 <amwass4@hotmail.com>
2024-01-17 20:16:35 +0100

    1:01:29

--- src/components/Countries.js
+++ src/components/Countries.js
@@ -1,55 +1,58 @@
+import React, { useEffect, useState } from "react";
 import axios from "axios";
-import React, { useState } from "react";
-import { useEffect } from "react";
 import Card from "./Card";
+import { v4 as uuidv4 } from 'uuid';
 
 const Countries = () => {
-  //  cette variable c'est comme si elle est stockée dans cette data, il faut la déclarer comme ca. C'est comme ca qu'on fait des variables
   const [data, setData] = useState([]);
   const [rangeValue, setRangeValue] = useState(36);
-  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
   const [selectedRadio, setSelectedRadio] = useState("");
-  //  Le useEffect se joue lorsque rapidement , il fait cloisonné les choses. C'est des Hook
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceania"];
+
+  // Le useEffect se joue lorsque le composant est monté
   useEffect(() => {
     axios
       .get("https://restcountries.com/v3.1/all")
       .then((res) => setData(res.data));
+      
   }, []);
 
   return (
     <div className="countries">
-        <ul className="radio-container">
-            <input 
-            type="range" 
-            min="1" 
-            max="250" 
-            defaultValue={rangeValue} 
-            onChange={(e) => setRangeValue(e.target.value)}/>
-            {radios.map((continent) => (
-                <li>
-                    <input 
-                    type="radio"  
-                    id={continent}
-                    name="continentRadio" 
-                    checked={continent === selectedRadio}
-                    onChange={(e) => setSelectedRadio(e.target.id)} />
-                    <label htmlFor={continent} >{continent}</label>
-                </li>
-            ))}
-                    </ul>
-         {selectedRadio &&   (
-            <button onClick={() => setSelectedRadio("") }> Annuler la recherche </button>
-          )}
-        
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li key={continent}>
+          <input
+            type="radio"
+            id={continent}
+            name="continentRadio"
+            checked={continent === selectedRadio}
+            onChange={(e) => setSelectedRadio(e.target.id)}
+          />
+          <label htmlFor={continent}>{continent}</label>
+        </li>
+        ))}
+      </ul>
+      {selectedRadio && (
+        <button onClick={() => setSelectedRadio("")}>
+          Annuler la recherche
+        </button>
+      )}
       <ul>
-        {/* Limiter la selection avec la methode Slice */}
-        
-        {data
+  
+      {data
           .filter((country) => country.continents[0].includes(selectedRadio))
           .sort((a, b) => b.population - a.population)
           .slice(0, rangeValue)
           .map((country) => (
-            <Card key={country.name.common} country={country} />
+            <Card key={uuidv4()} country={country} /> 
           ))}
 
       </ul>
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -2,25 +2,39 @@ import React, { useEffect, useState } from "react";
 import Navigation from "../components/Navigation";
 import Logo from "../components/Logo";
 import axios from "axios";
-import { getMouseEventOptions } from "@testing-library/user-event/dist/utils";
+import Article from "../components/Article";
 
 const Blog = () => {
+  const [blogData, setBlogData] = useState([]);
+  const [author, setAuthor] = useState("");
   const [content, setContent] = useState("");
   const [error, setError] = useState(false);
   // Aller chercher dans la DB.json
+
   const getData = () => {
-    axios.get("http://localhost:3004/articles").then((res) => console.log(res));
+    //  Simulation de serveur
+    axios
+      .get("http://localhost:3004/articles")
+      .then((res) => setBlogData(res.data));
   };
   useEffect(() => getData(), []);
 
   const handleSubmit = (e) => {
     // pour ne pas recharger la page
     e.preventDefault();
-
     if (content.length < 140) {
       setError(true);
     } else {
+      axios.post("http://localhost:3004/articles", {
+        author: "",
+        content : "",
+        date: Date.now(),
+      });
       setError(false);
+      setAuthor("");
+      setContent("");
+    //   reactualiser le blogdata
+      getData();
     }
   };
   return (
@@ -30,19 +44,31 @@ const Blog = () => {
       <h1> Blog </h1>
 
       <form onSubmit={(e) => handleSubmit(e)}>
-        <input type="text" placeholder="Nom" />
+        <input
+          type="text"
+          placeholder="Nom"
+          onChange={(e) => setAuthor(e.target.value)}
+          value={author}
+        />
         {/* Combien de caractere on été mis a l'interieur et renvoyer une erreur si besoin */}
         <textarea
           // Affichage conditionnel du style des border en cas de > 140 caracteres
           style={{ border: error ? "1px solid red" : "1px solid #61dafb" }}
           placeholder="Message"
-          onChangeCapture={(e) => setContent(e.target.value)}
+          onChange={(e) => setContent(e.target.value)}
+          value={content}
         ></textarea>
         {/* Afichage conditionnel  */}
         {error && <p>Veuillez écrire un minimum de 140 caractères</p>}
         <input type="submit" value="Envoyer" />
       </form>
-      <ul></ul>
+      <ul>
+        {blogData
+          .sort((a, b) => b.date - a.date)
+          .map((article) => (
+            <Article key={article.id} article={article} />
+          ))}
+      </ul>
     </div>
   );
 };
